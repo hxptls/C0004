66a05ddf5bfcb7192cc0b8ccba3422fa562cd335
Hexapetalous <hexapetalous@126.com>
2016-01-11 11:22:16 +0800

    C0004 started.

---
+++ doorman.py
@@ -0,0 +1,10 @@
+#
+# doorman.py
+# Created by Hexapetalous on Jan 11, 2016.
+#
+# This is a part of C0004, a new version of Tenzor's entrance gaurd.
+# This is the whole.
+#
+# Copyright (c) 2016 Hexapetalous. All rights reserved.
+# This code is licensed under MIT License.
+#
